cdf9b2fd6250be5ff615c165dd6a603e57afc444
Conejo <gabrielgconejo@gmail.com>
2016-04-27 08:44:58 -0300

    CanseiDeCommitarAqui

--- ParserLL/make.sh
+++ ParserLL/make.sh
@@ -4,22 +4,23 @@ gcc -o prog ParserLL.c
 2>>fuckoff; #relatórios de erro para arquivo fuckoff
 echo "Arquivo de entrada 1."
 ./prog <exemplo;
-sleep 3;
+sleep 2;
 echo "Arquivo de entrada 2."
 ./prog <exemplo2;
-sleep 3;
+sleep 2;
+echo "Arquivo de entrada 3."
 ./prog <exemplo3;
-echo "Arquivo de entrada 1."
-sleep 3;
+sleep 2;
+echo "Arquivo de entrada 4."
 ./prog <exemplo4;
-echo "Arquivo de entrada 1."
-sleep 3;
+sleep 2;
+echo "Arquivo de entrada 5."
 ./prog <exemplo5;
-echo "Arquivo de entrada 1."
-sleep 3;
+sleep 2;
+echo "Arquivo de entrada 6."
 ./prog <exemplo6;
-echo "Arquivo de entrada 1."
-sleep 3;
+sleep 2;
+echo "Arquivo de entrada 7."
 ./prog <exemplo7;
 rm prog;
 echo "Fim das entradas."
